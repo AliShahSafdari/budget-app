003e8c8d62b636267a9d6e826e5adca7f4cf8c5a
AliShahSafdari <alishahsafdari905@gmail.com>
2023-09-14 15:59:53 +0430

    Update expense file

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,7 +1,9 @@
 class Expense < ApplicationRecord
   # Associations
   belongs_to :author, class_name: 'User'
-  has_many :group_expenses
+  has_many :group_expenses, dependent: :destroy
+  has_many :groups, through: :group_expenses
+
 
   # Attributes and Validations
   validates :name, presence: true
